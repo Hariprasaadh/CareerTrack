7e39f26841b4640f9103ff9b2fd450e231caf209
Hariprasaadh <hariprasaadh.k2023@vitstudent.ac.in>
2025-02-10 23:51:49 +0530

    update pdf

--- AI/ScholarSphere/bot.py
+++ AI/ScholarSphere/bot.py
@@ -9,9 +9,11 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
+GOOGLE_API_KEY=st.secrets['GROQ_API_KEY']
+
 
 load_dotenv()
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv(GOOGLE_API_KEY))
 
 
 def get_pdf_text(pdf_docs):
